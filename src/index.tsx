dbdf4a86659a810090b3d3030f3d27074248fa2f
STsuchida <Saulo.Tsuchida@cignium.com>
2018-04-04 18:27:03 -0500

    This is a test

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,11 @@ import {Hello} from "./components/Hello";
 
 import "./style.scss"
 
+let catalogLocation : [number, string, boolean] = [1, "Test", true];
+catalogLocation[5] = "Test2";
+catalogLocation[3] = "Test2";
+catalogLocation[4] = 10;
+
 ReactDOM.render(
   <Hello compiler="Typescript" framework="React" />,
   document.getElementById("example")
